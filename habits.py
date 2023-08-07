84066b26a1ac70f60830d6b956de5774984e686f
dusk-and-dawn <david.rotnicki@gmail.com>
2023-08-07 08:10:13 +0200

    lets do this

---
+++ habits.py
@@ -0,0 +1,24 @@
+
+
+class habit: 
+    def __init__(self, name:str, description:str, period='daily'):
+        self.name = name 
+        self.description = description
+        self.period = period 
+
+    habits_lst = []
+
+    def record_activity(): 
+        print('test')
+
+    def delete_activity():
+        pass 
+
+    def change_date():
+        pass
+
+    def set_up_habit():
+        
+        habits_lst.append(x)
+
+habit.record_activity()
